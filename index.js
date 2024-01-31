819ff2e83135e6f78f198faa840b62c6287f951b
DevilData <devildata@hotmail.com>
2024-01-31 21:05:27 +0700

    feat:service add

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 
 console.log("Saya adalah Code-code Kesalahan")
 console.log("Dalam Code-code terdalam menunggu celah mematikan")
+console.log("Dalam Code-code terdalam menunggu celah mematikan")
---
+++ service.js
@@ -0,0 +1,4 @@
+//Project Git Pertama DevilData
+
+console.log("Saya adalah Code-code Kesalahan")
+console.log("Dalam Code-code terdalam menunggu celah mematikan")
