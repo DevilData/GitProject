dbcd877b738cbc042861248614419e8e73af8c33
DevilData <devildata@hotmail.com>
2024-01-31 22:00:27 +0700

    feat: add exercise0x03

---
+++ Exercise0x03.js
@@ -0,0 +1,53 @@
+//Code Untuk Membuat list Perkalian
+console.log("1.Code Perkalian 9 dengan N")
+
+let num1 = 9
+
+for (let i=1; i <=10; i++){
+    let hasil = num1 * i
+    console.log(`${num1} x ${i} = ${hasil}`)
+
+}
+
+//Code Palindrome
+console.log("\n2.Checker Palindrome")
+
+// function isPalindrome(str){
+//     let reverseStr = str.split('').reverse().join('')
+    
+//     if (str === reverseStr) {
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+// let sampleStr = "madam"
+// if (isPalindrome(sampleStr)) {
+//     console.log(`"${sampleStr}" adalah palindrome`)
+// }else{
+//     console.log(`"${sampleStr}" bukan palindrome`)
+// }
+
+//Care Kedua
+let name = "aries";
+let resName = "";
+
+for (let i = name.length-1; i >= 0; i--){
+    resName = resName + name[i]
+}
+
+console.log(resName)
+
+//Centimeter to Kilometer Converter
+console.log("\n3.Converter Centimeter ke Kilometer")
+
+let cm = 100000
+let km =  cm / 100000
+    console.log(`${cm} Centimeter samadengan  ${km} Kilometer`)
+
+//Number to Currency
+console.log("\n4.Currency Formatter")
+let  number = 1000
+let currencyFormatter = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' })
+console.log(currencyFormatter.format(number))
