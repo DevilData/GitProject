c8928e78f0f0688875f9ea7ce0c45e752acb9adf
DevilData <devildata@hotmail.com>
2024-02-07 02:51:16 +0700

    Reloaded

---
+++ Exercise0x06/0x01-Student.js
@@ -0,0 +1,16 @@
+//Waktunya Object Oriented Progamming
+
+// Class Student
+
+function Student {
+    constructor(name, email, age, score) {
+        this.name = name
+        this.email = email;
+        this.age = age;
+        this.score = score;
+ }
+
+let stu01 = new Student("Ahmde", "Ahmed@Ja.el",  23, 85);
+console.log(stu01);
+
+//Method Pertambahan
